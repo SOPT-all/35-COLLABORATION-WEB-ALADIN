4cd7c7f1fc8ea9bb0936aee1d4ebd9119b27ee95
Minn-Choi <min031106@dgu.ac.kr>
2024-11-18 02:39:18 +0900

    fix: 테스트용으로 설정한 배경 삭제

--- src/pages/Main/Main.styled.ts
+++ src/pages/Main/Main.styled.ts
@@ -2,17 +2,4 @@ import styled from "@emotion/styled";
 
 export const MainWrapper = styled.div`
   ${props => props.theme.fonts.head_b_24};
-  display: flex;
-  width: 200px;
-  margin: 10px;
-  padding: 10px;
-  border-radius: 100%;
-
-  background-color: black;  
-
-  color: #e9ecef;
-  font-weight: bold;
-  font-size: 14px; 
-
-  animation: ease;
 `;
