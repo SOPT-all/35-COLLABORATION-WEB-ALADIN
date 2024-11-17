582e0eb8daa25d78c89b83a8e721960803dbef07
Minn-Choi <min031106@dgu.ac.kr>
2024-11-18 02:30:16 +0900

    init: GlobalStyle, theme 세팅 및 stylelint 설정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,9 +1,173 @@
-import { Global,css } from "@emotion/react";
+/** @jsxImportSource @emotion/react */
+import { Global, css } from "@emotion/react";
 
-export const globalStyle = css`
-  //여기에 전역 상태 정의!
-`
+export const globalStyles = css`
+  * {
+    box-sizing: border-box;
+  }
 
-const GlobalStyle = () => <Global styles={globalStyle}/>
+  body,
+  button,
+  dd,
+  dl,
+  dt,
+  fieldset,
+  form,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  input,
+  legend,
+  li,
+  ol,
+  p,
+  select,
+  table,
+  td,
+  textarea,
+  th,
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  body,
+  button,
+  input,
+  select,
+  table,
+  textarea {
+    font-family: "AppleSDGothicNeoH00", "AppleSDGothicNeoB00", "AppleSDGothicNeoM00", 
+      "AppleSDGothicNeoR00", "AppleSDGothicNeoSB00", -apple-system, BlinkMacSystemFont,
+      "Malgun Gothic", "맑은 고딕", helvetica, sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-size: inherit;
+    line-height: inherit;
+  }
+
+  textarea {
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    background-color: transparent;
+    border: 0;
+    word-break: keep-all;
+    word-wrap: break-word;
+  }
+
+  button,
+  input {
+    border-radius: 0;
+    border: 0;
+  }
+
+  button {
+    background-color: transparent;
+  }
+
+  fieldset,
+  img {
+    border: 0;
+  }
+
+  img {
+    vertical-align: top;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  address,
+  em {
+    font-style: normal;
+  }
+
+  a {
+    display: flex;
+    text-decoration: none;
+  }
+
+  iframe {
+    overflow: hidden;
+    margin: 0;
+    border: 0;
+    padding: 0;
+    vertical-align: top;
+  }
+
+  mark {
+    background-color: transparent;
+  }
+
+  i {
+    font-style: normal;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    min-height: 100vh;
+  }
+
+  @font-face {
+    font-family: "AppleSDGothicNeoH00";
+    src: url("/fonts/AppleSDGothicNeoH.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "AppleSDGothicNeoB00";
+    src: url("/fonts/AppleSDGothicNeoB.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "AppleSDGothicNeoR00";
+    src: url("/fonts/AppleSDGothicNeoR.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "AppleSDGothicNeoM00";
+    src: url("/fonts/AppleSDGothicNeoM.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: "AppleSDGothicNeoSB00";
+    src: url("/fonts/AppleSDGothicNeoSB.ttf") format("truetype");
+  }
+
+  html {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    -webkit-touch-callout: none;
+    -webkit-tap-highlight-color: rgb(0 0 0 / 0%);
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-size: 62.5%;
+    scrollbar-width: none;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    scroll-behavior: smooth;
+  }
+`;
+
+const GlobalStyle = () => <Global styles={globalStyles} />;
 
 export default GlobalStyle;
