2746d8ea66d76d6a832f2ea5f5b48f04c40944cd
gahyeon <gus_05@naver.com>
2024-11-21 21:55:48 +0900

    feat: 체크박스 버튼 구현

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,4 @@
-export { default as BtnFFloating } from "./BtnFFloating";
-export { default as BtnFloating } from "./BtnFloating";
-export { default as Hihi } from "./Hihi";
+export { default as BtnFFloating } from './BtnFFloating';
+export { default as BtnFloating } from './BtnFloating';
+export { default as Hihi } from './Hihi';
+export { default as IcCheckbox } from './IcCheckbox';
