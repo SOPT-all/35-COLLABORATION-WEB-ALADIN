2bec15a3f488de5957c343582632ec4116251f80
ocahs9 <piorag99@gmail.com>
2024-11-22 17:14:58 +0900

    fix: 빌드와 관련된 내용들 ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@assets", replacement: "/src/assets" },
-      { find: "@pages", replacement: "/src/pages" },
-      { find: "@components", replacement: "/src/components" },
-      { find: "@styles", replacement: "/src/styles" },
-      { find: "@constants", replacement: "/src/constants" },
-      { find: "@hooks", replacement: "/src/hooks" },
-      { find: "@utils", replacement: "/src/utils" },
-      { find: "@apis", replacement: "/src/apis" },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@styles', replacement: '/src/styles' },
+      { find: '@constants', replacement: '/src/constants' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@utils', replacement: '/src/utils' },
+      { find: '@apis', replacement: '/src/apis' },
     ],
   },
-})
+});
