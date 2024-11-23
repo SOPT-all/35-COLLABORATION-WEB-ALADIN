3fe23e788e4f0dd6cee153f0024c9be250fd65a8
zzz-myam <jiyoo0315@naver.com>
2024-11-23 21:41:37 +0900

    init: 작업 전 초기 세팅

---
+++ src/components/Main/HomeSection8/HomeSection8.tsx
@@ -0,0 +1,11 @@
+import * as S from './HomeSection8.styled';
+
+const HomeSection8 = () => {
+  return (
+    <>
+      <S.HomeSection8Wrapper>HomeSection8</S.HomeSection8Wrapper>;
+    </>
+  );
+};
+
+export default HomeSection8;
