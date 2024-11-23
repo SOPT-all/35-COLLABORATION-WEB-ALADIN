a2c7a333d9222686f2804a7a894f2c40984ffa99
gahyeon <gus_05@naver.com>
2024-11-23 20:58:06 +0900

    feat: 장바구니 quantity control btn 컴포넌트 구현

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,5 +1,7 @@
 export { default as BtnFFloating } from './BtnFFloating';
 export { default as BtnFloating } from './BtnFloating';
 export { default as Hihi } from './Hihi';
+export { default as IcAdd } from './IcAdd';
 export { default as IcCheckbox } from './IcCheckbox';
 export { default as IcLocation } from './IcLocation';
+export { default as IcRemove } from './IcRemove';
