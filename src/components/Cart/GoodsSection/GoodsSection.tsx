6d0a74c965a9c7d8bc325d4c43f6f0443c49e50b
gahyeon <gus_05@naver.com>
2024-11-26 00:52:42 +0900

    feat: cart section 3 퍼블리싱

---
+++ src/components/Cart/GoodsSection/GoodsSection.tsx
@@ -0,0 +1,69 @@
+import {
+  IcLeftarrowMediumGray50,
+  IcRightarrowMediumGray50,
+  ImgShopx2,
+} from '@assets/svgs';
+import * as S from './Goods.styled';
+import { GOODS_INFO } from '@constants/GoodsInfoConstants';
+
+const GoodsSection = () => {
+  return (
+    <S.GoodsInfoWrapper>
+      <S.GoodsInfoContainer>
+        <S.TitleBox>
+          <S.Title>
+            <h1>
+              <span>알라딘</span> <span className="purple">굿즈</span>
+            </h1>
+          </S.Title>
+          <S.ButtonBox>
+            <S.PrevBtn>
+              <IcLeftarrowMediumGray50 />
+            </S.PrevBtn>
+            <S.NextBtn>
+              <IcRightarrowMediumGray50 />
+            </S.NextBtn>
+          </S.ButtonBox>
+        </S.TitleBox>
+        <S.GoodsList>
+          {GOODS_INFO.slice(0, 4).map((goods) => (
+            <S.GoodsInfo key={goods.id}>
+              <goods.image className="img-goods" />
+              <p>{goods.name}</p>
+            </S.GoodsInfo>
+          ))}
+        </S.GoodsList>
+      </S.GoodsInfoContainer>
+      <S.GoodsInfoContainer>
+        <S.TitleBox>
+          <S.Title>
+            <h1>
+              <span>알라딘</span> <span className="purple">굿즈</span>
+            </h1>
+          </S.Title>
+          <S.ButtonBox>
+            <S.PrevBtn>
+              <IcLeftarrowMediumGray50 />
+            </S.PrevBtn>
+            <S.NextBtn>
+              <IcRightarrowMediumGray50 />
+            </S.NextBtn>
+          </S.ButtonBox>
+        </S.TitleBox>
+        <S.GoodsList>
+          {GOODS_INFO.slice(4, 8).map((goods) => (
+            <S.GoodsInfo key={goods.id}>
+              <S.Img>
+                <ImgShopx2 className="shop-icon" />
+                <goods.image className="img-goods" />
+              </S.Img>
+              <p>{goods.name}</p>
+            </S.GoodsInfo>
+          ))}
+        </S.GoodsList>
+      </S.GoodsInfoContainer>
+    </S.GoodsInfoWrapper>
+  );
+};
+
+export default GoodsSection;
---
+++ src/constants/GoodsInfoConstants.ts
@@ -0,0 +1,61 @@
+import {
+  ImgCart31,
+  ImgCart32,
+  ImgCart33,
+  ImgCart34,
+  ImgCart35,
+  ImgCart36,
+  ImgCart37,
+  ImgCart38,
+} from '@assets/svgs';
+import { JSX } from 'react';
+import React from 'react';
+
+export interface Goods {
+  id: number;
+  image: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
+  name: string;
+}
+
+export const GOODS_INFO: Goods[] = [
+  {
+    id: 1,
+    image: ImgCart31,
+    name: '밑줄 긋기 쉬운 휘어지는 자\n(20cm)',
+  },
+  {
+    id: 2,
+    image: ImgCart32,
+    name: '전통 문양 책갈피',
+  },
+  {
+    id: 3,
+    image: ImgCart33,
+    name: '글자를 가리지 않는 스티키 북마크\n(40매)',
+  },
+  {
+    id: 4,
+    image: ImgCart34,
+    name: '본투리드 초저점도 3색 볼펜 0.7mm',
+  },
+  {
+    id: 5,
+    image: ImgCart35,
+    name: '콜드브루 디카페인 날개',
+  },
+  {
+    id: 6,
+    image: ImgCart36,
+    name: '드립백 에티오피아 예가체프 하루수케',
+  },
+  {
+    id: 7,
+    image: ImgCart37,
+    name: '진드립백 가을하다',
+  },
+  {
+    id: 8,
+    image: ImgCart38,
+    name: '캡슐 파우스트',
+  },
+];
